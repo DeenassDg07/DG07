c1172588f8f712c7470461c657ca78b27701775e
DeenassDg07 <astrakovmax01@gmail.com>
2026-01-28 16:09:20 +1000

    DG07

---
+++ resources/views/Registration.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registration</title>
+    @vite([
+    'resources/css/test.css'
+])
+</head>
+<body>
+<div class="Registration">
+    <div class="header">Зарегестрироваться</div>
+    <div class="xxx">
+    <input placeholder="Логин:">
+    <input placeholder="Пароль:">
+        <input placeholder="Повторите пароль:">
+    </div>
+
+    <div class="xx1">
+        <button>
+            Зарегистрироваться
+        </button>
+    </div>
+
+    <div class="test">test</div>
+
+</div>
+</body>
+</html>
